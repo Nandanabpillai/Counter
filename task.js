36e94d9817ee770259d2d4a5fa3a052b0836f595
Nandana B Pillai <nandanabpillai2003@gmail.com>
2022-12-02 18:58:14 +0530

    Javascript file

---
+++ task.js
@@ -0,0 +1,13 @@
+let num = 50;
+
+function increment()
+{
+  num++;
+  document.getElementById("demo").innerHTML = num;
+}
+
+function decrement()
+{
+  num--;
+  document.getElementById("demo").innerHTML = num;
+}
